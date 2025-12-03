6fa43ee6cd238315941510f5d0107e5f06d8a020
Aulia Iskandar Lubis <auliaiskandarlubis31@gmail.com>
2025-12-03 14:51:46 +0700

    modified:   sdiamfatwa/urls.py

--- sdiamfatwa/urls.py
+++ sdiamfatwa/urls.py
@@ -1,5 +1,3 @@
-# sdiamfatwa/urls.py
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -14,24 +12,25 @@ sitemaps = {
 }
 
 urlpatterns = [
-    # Path Sitemap Dulu
+    # 1. Path Sitemap
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
 
     path('admin/', admin.site.urls),
     
-    # Path untuk apps
-    path('', include('home.urls')),
-    path('berita/', include('berita.urls')),
-    path('agenda/', include('agenda.urls')),
-    path('galeri/', include('galeri.urls')),
-    path('kontak/', include('kontak.urls')),
+    # 2. Path Apps - Diberi namespace eksplisit untuk memastikan reverse() berhasil
+    # path('', include('home.urls')), # <-- DIGANTI DENGAN VERSI DI BAWAH INI
+    path('', include(('home.urls', 'home'), namespace='home')),
+    path('berita/', include(('berita.urls', 'berita'), namespace='berita')),
+    path('agenda/', include(('agenda.urls', 'agenda'), namespace='agenda')),
+    path('galeri/', include(('galeri.urls', 'galeri'), namespace='galeri')),
+    path('kontak/', include(('kontak.urls', 'kontak'), namespace='kontak')),
     
     # Path CKEditor dan PWA
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('pwa.urls')),
 
-    # Path Media dan Static (Biasanya hanya dipakai untuk Production/Staging tanpa Nginx/Apache)
+    # Path Media dan Static (Seperti konfigurasi kamu sebelumnya)
     re_path(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
     }),
