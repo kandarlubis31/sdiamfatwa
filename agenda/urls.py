a10dcb921ab0efd218c557dcc21d3b55a8f161c4
Aulia Iskandar Lubis <auliaiskandarlubis31@gmail.com>
2025-12-03 14:19:48 +0700

    modified:   agenda/urls.py 	modified:   berita/urls.py 	modified:   galeri/urls.py 	modified:   home/urls.py 	modified:   kontak/urls.py

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'agenda'
+
 urlpatterns = [
     path('', views.agenda_list, name='agenda_list'),
     path('<int:id>/', views.agenda_detail, name='agenda_detail'),
